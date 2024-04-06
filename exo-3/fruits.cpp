892bbd4ee2014f3bb618f6fc0bb54e24e7527e69
David Lafontant <lafontant.david@gmail.com>
2024-04-06 02:30:55 -0400

    Exo 3

---
+++ exo-3/fruits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+  // declare variables
+  int apples = 20;
+  int oranges = 18;
+  int nbr_of_fruits = apples + oranges;
+
+  std::cout<<"Oranges \t: " << oranges << std::endl;
+  std::cout<<"Apples \t: " << apples << std::endl;
+  std::cout<<"Number of fruits \t: " << nbr_of_fruits << std::endl;
+
+  //return
+  return 0;
+}
