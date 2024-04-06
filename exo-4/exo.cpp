554d37825266accd2f95e4b488d8eed53cf83005
David Lafontant <lafontant.david@gmail.com>
2024-04-06 02:31:30 -0400

    Exo 4

---
+++ exo-4/exo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(){
+  // declare && initialize a character array
+  char greeting[] = "Hello, World!";
+
+  // display array index 2
+  std::cout<< greeting[2] << std::endl;
+
+  return 0;
+}
